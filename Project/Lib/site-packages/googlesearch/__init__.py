a05c1348f57582805005004e5576711b1139674d
Katakam Karthik <karthik23052001@gmail.com>
2021-12-09 15:09:08 +0530

    Feature extraction and Model creation

---
+++ Project/Lib/site-packages/googlesearch/__init__.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+
+def search(term, num_results=10, lang="en", proxy=None):
+    usr_agent = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/61.0.3163.100 Safari/537.36'}
+
+    def fetch_results(search_term, number_results, language_code):
+        escaped_search_term = search_term.replace(' ', '+')
+
+        google_url = 'https://www.google.com/search?q={}&num={}&hl={}'.format(escaped_search_term, number_results+1,
+                                                                              language_code)
+        proxies = None
+        if proxy:
+            if proxy[:5]=="https":
+                proxies = {"https":proxy} 
+            else:
+                proxies = {"http":proxy}
+        
+        response = get(google_url, headers=usr_agent, proxies=proxies)    
+        response.raise_for_status()
+
+        return response.text
+
+    def parse_results(raw_html):
+        soup = BeautifulSoup(raw_html, 'html.parser')
+        result_block = soup.find_all('div', attrs={'class': 'g'})
+        for result in result_block:
+            link = result.find('a', href=True)
+            title = result.find('h3')
+            if link and title:
+                yield link['href']
+
+    html = fetch_results(term, num_results, lang)
+    return list(parse_results(html))
---
+++ Project/OLD/FeatureExtraction.py
@@ -0,0 +1,313 @@
+#importing required packages for this module
+import pandas as pd
+from googlesearch import search
+import re
+from bs4 import BeautifulSoup
+import whois
+import urllib
+import urllib.request
+from datetime import datetime
+import requests
+
+#loading the phishing URLs data to dataframe
+data0 = pd.read_csv("2.online-valid.csv")
+
+#Collecting 5,000 Phishing URLs randomly
+phishurl = data0.sample(n = 5000, random_state = 12).copy()
+phishurl = phishurl.reset_index(drop=True)
+
+#Loading legitimate files 
+data1 = pd.read_csv("1.Benign_list_big_final.csv")
+data1.columns = ['URLs']
+
+#Collecting 5,000 Legitimate URLs randomly
+legiurl = data1.sample(n = 5000, random_state = 12).copy()
+legiurl = legiurl.reset_index(drop=True)
+
+# importing required packages for this section
+from urllib.parse import urlparse,urlencode
+import ipaddress
+import re
+
+# 1.Domain of the URL (Domain) 
+def get_Domain(url):  
+  domain = urlparse(url).netloc
+  #print(domain)
+  if re.match(r"^www.",domain):
+	       domain = domain.replace("www.","")
+  return domain
+
+# 2.Checks for IP address in URL (Have_IP)
+def having_IP(url):
+  try:
+    ipaddress.ip_address(url)
+    ip = 1
+  except:
+    ip = 0
+  return ip
+
+# 3.Checks the presence of @ in URL (Have_At)
+def having_ampersand  (url):
+  if "@" in url:
+    at = 1    
+  else:
+    at = 0    
+  return at
+
+# 4.Finding the length of URL and categorizing (URL_Length)
+def check_Length(url):
+  if len(url) < 54:
+    length = 0            
+  else:
+    length = 1            
+  return length
+
+# 5.Gives number of '/' in URL (URL_Depth)
+def getDepth(url):
+  s = urlparse(url).path.split('/')
+  depth = 0
+  for j in range(len(s)):
+    if len(s[j]) != 0:
+      depth = depth+1
+  return depth
+
+def redirection(url):
+  pos = url.rfind('//')
+  if pos > 6:
+    if pos > 7:
+      return 1
+    else:
+      return 0
+  else:
+    return 0
+
+# 7.Existence of “HTTPS” Token in the Domain Part of the URL (https_Domain)
+def httpDomainCheck(url):
+  domain = urlparse(url).netloc
+  if 'https' in domain:
+    return 1
+  elif 'http' in domain:
+    return 1
+  else:
+    return 0
+
+#listing shortening services
+shortening_services = r"bit\.ly|goo\.gl|shorte\.st|go2l\.ink|x\.co|ow\.ly|t\.co|tinyurl|tr\.im|is\.gd|cli\.gs|" \
+                      r"yfrog\.com|migre\.me|ff\.im|tiny\.cc|url4\.eu|twit\.ac|su\.pr|twurl\.nl|snipurl\.com|" \
+                      r"short\.to|BudURL\.com|ping\.fm|post\.ly|Just\.as|bkite\.com|snipr\.com|fic\.kr|loopt\.us|" \
+                      r"doiop\.com|short\.ie|kl\.am|wp\.me|rubyurl\.com|om\.ly|to\.ly|bit\.do|t\.co|lnkd\.in|db\.tt|" \
+                      r"qr\.ae|adf\.ly|goo\.gl|bitly\.com|cur\.lv|tinyurl\.com|ow\.ly|bit\.ly|ity\.im|q\.gs|is\.gd|" \
+                      r"po\.st|bc\.vc|twitthis\.com|u\.to|j\.mp|buzurl\.com|cutt\.us|u\.bb|yourls\.org|x\.co|" \
+                      r"prettylinkpro\.com|scrnch\.me|filoops\.info|vzturl\.com|qr\.net|1url\.com|tweez\.me|v\.gd|" \
+                      r"tr\.im|link\.zip\.net"
+# 8. Checking for Shortening Services in URL (Tiny_URL)
+def tinyURL(url):
+    match=re.search(shortening_services,url)
+    if match:
+        return 1
+    else:
+        return 0
+
+# 9.Checking for Prefix or Suffix Separated by (-) in the Domain (Prefix/Suffix)
+def prefixSuffix(url):
+    if '-' in urlparse(url).netloc:
+        return 1            # phishing
+    else:
+        return 0            # legitimate
+
+
+
+
+# 12.Web traffic (Web_Traffic)
+def web_traffic(url):
+  try:
+    #Filling the whitespaces in the URL if any
+    url = urllib.parse.quote(url)
+    rank = BeautifulSoup(urllib.request.urlopen("http://data.alexa.com/data?cli=10&dat=s&url=" + url).read(), "xml").find(
+        "REACH")['RANK']
+    rank = int(rank)
+  except TypeError:
+        return 1
+  if rank <100000:
+    return 1
+  else:
+    return 0
+
+# 13.Survival time of domain: The difference between termination time and creation time (Domain_Age)  
+def domainAge(domain_name):
+  creation_date = domain_name.creation_date
+  expiration_date = domain_name.expiration_date
+  if (isinstance(creation_date,str) or isinstance(expiration_date,str)):
+    try:
+      creation_date = datetime.strptime(creation_date,'%Y-%m-%d')
+      expiration_date = datetime.strptime(expiration_date,"%Y-%m-%d")
+    except:
+      return 1
+  if ((expiration_date is None) or (creation_date is None)):
+      return 1
+  elif ((type(expiration_date) is list) or (type(creation_date) is list)):
+      return 1
+  else:
+    ageofdomain = abs((expiration_date - creation_date).days)
+    if ((ageofdomain/30) < 6):
+      age = 1
+    else:
+      age = 0
+  return age
+
+# 14.End time of domain: The difference between termination time and current time (Domain_End) 
+def domainEnd(domain_name):
+  expiration_date = domain_name.expiration_date
+  if isinstance(expiration_date,str):
+    try:
+      expiration_date = datetime.strptime(expiration_date,"%Y-%m-%d")
+    except:
+      return 1
+  if (expiration_date is None):
+      return 1
+  elif (type(expiration_date) is list):
+      return 1
+  else:
+    today = datetime.now()
+    end = abs((expiration_date - today).days)
+    if ((end/30) < 6):
+      end = 0
+    else:
+      end = 1
+  return end
+
+# importing required packages for this section
+
+# 15. IFrame Redirection (iFrame)
+def iframe(response):
+  if response == "":
+      return 1
+  else:
+      if re.findall(r"[<iframe>|<frameBorder>]", response.text):
+          return 0
+      else:
+          return 1
+
+# 16.Checks the effect of mouse over on status bar (Mouse_Over)
+def mouseOver(response): 
+  if response == "" :
+    return 1
+  else:
+    if re.findall("<script>.+onmouseover.+</script>", response.text):
+      return 1
+    else:
+      return 0
+
+# 17.Checks the status of the right click attribute (Right_Click)
+def rightClick(response):
+  if response == "":
+    return 1
+  else:
+    if re.findall(r"event.button ?== ?2", response.text):
+      return 0
+    else:
+      return 1
+
+# 18.Checks the number of forwardings (Web_Forwards)    
+def forwarding(response):
+  if response == "":
+    return 1
+  else:
+    if len(response.history) <= 2:
+      return 0
+    else:
+      return 1
+
+def Links_pointing_to_page(response): 
+  if response == "":
+    return 1
+  else:
+    number_of_links = len(re.findall(r"<a href=", response.text))
+    if number_of_links == 0:
+      return 1
+    elif number_of_links <= 2:
+      return 1
+    else:
+      return 0
+
+def check_email(url):
+  try:
+    response = requests.get(url)
+  except:
+    response = ""
+  if response == "":
+    return 1
+  else:
+    if re.findall(r"[mail\(\)|mailto:?]", response.text):
+      return 1
+    else:
+      return 0
+
+
+def Google_Index(url):
+  site=search(url, 15)
+  if site:
+    temp=0
+  else:
+    temp=1
+  return temp
+
+#Function to extract features
+def featureExtraction(url,label):
+
+  features = []
+  #Address bar based features (10)
+  features.append(get_Domain(url))
+  features.append(having_IP(url))
+  features.append(having_ampersand(url))
+  features.append(check_Length(url))
+  features.append(getDepth(url))
+  features.append(redirection(url))
+  features.append(httpDomainCheck(url))
+  features.append(tinyURL(url))
+  features.append(web_traffic(url))
+  features.append(prefixSuffix(url))
+  features.append(Google_Index(url))
+  features.append(check_email(url))
+
+  #Domain based features (4)
+  dns = 0
+  try:
+    domain_name = whois.whois(urlparse(url).netloc)
+  except:
+    dns = 1
+
+  features.append(dns)
+  features.append(1 if dns == 1 else domainAge(domain_name))
+  features.append(1 if dns == 1 else domainEnd(domain_name))
+  
+  # HTML & Javascript based features (4)
+  try:
+    response = requests.get(url)
+  except:
+    response = ""
+  features.append(iframe(response))
+  features.append(mouseOver(response))
+  features.append(rightClick(response))
+  features.append(forwarding(response))
+  features.append(Links_pointing_to_page(response))
+  features.append(label)
+  
+  return features
+
+#Extracting the feautres & storing them in a list
+legi_features = []
+label = 0
+
+for i in range(0, 5000):
+  url = legiurl['URLs'][i]
+  legi_features.append(featureExtraction(url,label))
+
+#converting the list to dataframe
+feature_names = ['Domain', 'Have_IP', 'Have_At', 'URL_Length', 'URL_Depth','Redirection', 
+                      'https_Domain', 'TinyURL', 'Prefix/Suffix', 'Web_Traffic', 'Pointing_links','Google_index'
+                      'Domain_Age', 'Domain_End', 'iFrame', 'Mouse_Over','Right_Click', 'Web_Forwards','Email', 'Label']
+
+legitimate = pd.DataFrame(legi_features, columns= feature_names)
+
+  
